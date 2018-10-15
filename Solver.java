42d8154e49f23ff1b1e7c1ea57b6f8524cf9207b
Steve <sfs8@icloud.com>
2018-10-14 20:40:10 -0400

    round 2

---
+++ Solver.java
@@ -0,0 +1,26 @@
+public class Solver {
+    /*function BT(CSP csp){
+        return backtrack({}, CSP csp)
+    }
+
+    function backtrack(Assignment assignment, CSP csp){
+        if (assignment is complete){
+            return assignment;
+        }
+        var = SelectUnassignedVar(csp);
+        for(value : OrderDomainValues(var, assignment, csp)){
+            if(value is consistent with assignment){
+                add <var,value> to assignment
+                result = backtrack(assignment, csp)
+                if (result != failure){
+                    return result
+                }
+                else{
+                    remove <var,value> from assignment
+                }
+            }
+        }
+        return failure;
+    }
+    */
+}
